47f3d54441dd81f73695649b31694b757d35a77f
nsf <no.smile.face@gmail.com>
2012-07-11 03:11:41 +0600

    Add top-level help message. Better than nothing.

--- main.go
+++ main.go
@@ -4,6 +4,25 @@ import "os"
 import "github.com/nsf/torgo/view"
 import "github.com/nsf/torgo/make"
 import "github.com/nsf/torgo/client"
+import "runtime"
+import "fmt"
+
+const main_help = `This is a set of BitTorrent tools, which currently includes:
+
+torgo make:
+    Utility for making .torrent files. Type "torgo make -h" for usage info.
+
+torgo view:
+    Torrent file viewer. Type "torgo view -h" for usage info.
+
+torgo client:
+    Fully featured BitTorrent client. Not implemented yet.
+`
+
+func print_version() {
+	fmt.Printf("TorGo version 0.1, compiled with %s (Go version: %s)\n",
+		runtime.Compiler, runtime.Version())
+}
 
 func dispatch_command() {
 	switch os.Args[1] {
@@ -14,8 +33,12 @@ func dispatch_command() {
 	case "client":
 		client.Tool()
 	case "-h", "--help", "help":
+		print_version()
+		fmt.Print(main_help)
+	case "-v", "--version", "version":
+		print_version()
 	default:
-		println("unknown command: " + os.Args[1])
+		fmt.Fprintf(os.Stderr, "unknown command: %s", os.Args[1])
 		os.Exit(1)
 	}
 }
